a4ecfff48d750b5a9189565bf678c183fcf5d054
goodfor6 <1015401377@qq.com>
2017-05-23 22:20:49 +0800

    log4j

--- src/org/springframework/beans/BeanWrapperImpl.java
+++ src/org/springframework/beans/BeanWrapperImpl.java
@@ -18,6 +18,8 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
 import org.springframework.core.CollectionFactory;
 import org.springframework.core.GenericCollectionTypeResolver;
 import org.springframework.core.MethodParameter;
