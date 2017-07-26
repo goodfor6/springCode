193dc43d30680247c7e483f5d63ef0b476b43b76
goodfor6 <1015401377@qq.com>
2017-07-26 18:27:39 +0800

    good

---
+++ src/org/springframework/core/util/CommonsLogWriter.java
@@ -0,0 +1,13 @@
+package org.springframework.core.util;
+
+import org.apache.commons.logging.Log;
+
+/**
+ * Created by Administrator on 2017/7/26 0026.
+ */
+public class CommonsLogWriter  extends Writer{
+    private final Log logger;
+    private final StringBuilder buffer = new Strin
+
+
+}
