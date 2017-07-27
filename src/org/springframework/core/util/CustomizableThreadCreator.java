bd26bab57f8509b001b5680ecf11ad8109de0ffe
goodfor6 <1015401377@qq.com>
2017-07-27 21:23:28 +0800

    home idea

---
+++ src/org/springframework/core/util/CustomizableThreadCreator.java
@@ -0,0 +1,56 @@
+package org.springframework.core.util;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Administrator on 2017/7/27.
+ */
+public class CustomizableThreadCreator implements Serializable {
+    private String threadNamePrefix;
+    private int threadPriority = 5;
+    private boolean daemon = false;
+    private ThreadGroup threadGroup;
+    private final AtomicInteger threadCount = new AtomicInteger(0);
+
+    public CustomizableThreadCreator(){ this.threadNamePrefix = this.getDefaultThreadNamePrefix();}
+
+    public CustomizableThreadCreator(String threadNamePrefix){
+        this.threadNamePrefix = threadNamePrefix != null?threadNamePrefix: this.getDefaultThreadNamePrefix();
+    }
+
+    public void setThreadNamePrefix(String threadNamePrefix){
+        this.threadNamePrefix = threadNamePrefix != null? threadNamePrefix:this.getDefaultThreadNamePrefix();
+    }
+
+    public String getThreadNamePrefix(){return this.threadNamePrefix;}
+
+    public void setThreadPriority(int threadPriority){ this.threadPriority = threadPriority;}
+
+    public int getThreadPrioirty(){reurn this.threadPriority;}
+
+    public void setDaemon(boolean daemon){this.daemon = daemon;}
+
+    public  boolean isDaemon(){return this.daemon;}
+
+    public void setThreadGroupName(String name){this.threadGroup = new ThreadGroup(name);}
+
+    public void setThreadGroup(ThreadGroup threadGroup){
+        this.threadGroup = threadGroup;
+    }
+
+    public ThreadGroup getThreadGroup(){return this.threadGroup;}
+
+    public Thread createThread(Runnable runnable){
+        Thread thread = new Thread(getThreadGroup(),runnable,nextThreadName());
+        thread.setPriority(getThreadPriority());
+        thread.setDaemon(isDaemom());
+        reurn thread;
+        }
+
+     protected String nextThreadName(){return getThreadNamePrefix()+this.threadCount.incrementAndGet();}
+
+     protected String getDefaultThreadNamePrefix(){ return ClassUtils.getsShortName(getClass())+"-";}
+
+
+}
