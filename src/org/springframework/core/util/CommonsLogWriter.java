dd5000e21b1a93317a93ba64864d92e0d33e9e8b
goodfor6 <1015401377@qq.com>
2017-07-28 08:40:09 +0800

    通用日志输出

--- src/org/springframework/core/util/CommonsLogWriter.java
+++ src/org/springframework/core/util/CommonsLogWriter.java
@@ -1,5 +1,7 @@
 package org.springframework.core.util;
 
+import java.io.Writer;
+
 import org.apache.commons.logging.Log;
 
 /**
@@ -7,7 +9,43 @@ import org.apache.commons.logging.Log;
  */
 public class CommonsLogWriter  extends Writer{
     private final Log logger;
-    private final StringBuilder buffer = new Strin
+    private final StringBuilder buffer = new StringBuilder();
 
+    public CommonsLogWriter(Log logger){
+    	Assert.notNull(logger,"Logger must notbe null");
+    	this.logger = logger;
+    }
+    
+    public void write(char ch){
+    	if(ch == '\n' && this.buffer.length()> 0){
+    		this.logger.debug(this.buffer.toString());
+    		this.buffer.setLength(0);
+    	}
+    	else{
+    		this.buffer.append(ch);
+    	}
+    }
+    
+    public void write(char[] buffer,int offset,int length){
+    	for(int i = 0;i < length; i++){
+    		char ch = buffer[offset + i];
+    		if(ch == '\n' && this.buffer.length() > 0){
+    			this.logger.debug(this.buffer.toString());
+    			this.buffer.setLength(0);
+    		}
+    		else{
+    			this.buffer.append(ch);
+    		}
+    	}
+    }
+    
+    public void flush(){
+    	
+    }
+    
+    public void close(){
+    	
+    }
+    
 
 }
