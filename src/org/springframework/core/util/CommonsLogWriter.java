71e3e3c4489b3f80644275561dcaea3bee8f87e3
goodfor6 <1015401377@qq.com>
2017-08-02 09:04:33 +0800

    冲突

--- src/org/springframework/core/util/CommonsLogWriter.java
+++ src/org/springframework/core/util/CommonsLogWriter.java
@@ -2,10 +2,12 @@ package org.springframework.core.util;
 
 import org.apache.commons.logging.Log;
 
+import java.io.Writer;
+
 /**
  * Created by Administrator on 2017/7/26 0026.
  */
-public class CommonsLogWriter  extends Writer{
+public class CommonsLogWriter  extends Writer {
     private final Log logger;
     private final StringBuilder buffer = new StringBuilder();
 
