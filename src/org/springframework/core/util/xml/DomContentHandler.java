30654e3a48942cf4d45872ebf94fccdfa9e51ff3
goodfor6 <1015401377@qq.com>
2017-08-25 09:01:24 +0800

    dom 内容解析

--- src/org/springframework/core/util/xml/DomContentHandler.java
+++ src/org/springframework/core/util/xml/DomContentHandler.java
@@ -3,10 +3,9 @@ package org.springframework.core.util.xml;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.xml.sax.Locator;
 import org.springframework.core.util.Assert;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
+import org.w3c.dom.*;
 import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
@@ -52,5 +51,41 @@ public class DomContentHandler  implements ContentHandler{
 		element = (Element)parent.appendChild(element);
 		elements.add(element);
 	}
+
+	public void endElement(String uri, String localName, String qName)throws SAXException{
+		elements.remove(elements.size() - 1);
+	}
+
+	public void characters(char ch[], int start, int length)throws SAXException{
+		String data = new String(ch, start, length);
+		Node parent = getParent();
+		Node lastChild = parent.getLastChild();
+		if(lastChild != null && lastChild.getNodeType() == Node.TEXT_NODE){
+			((Text)lastChild).appendData(data);
+		}
+		else{
+			Text text = document.createTextNode(data);
+			parent.appendChild(text);
+		}
+
+	}
+
+	public void processingInstruction(String target, String data)throws SAXException{
+		Node parent = getParent();
+		ProcessingInstruction pi = document.createProcessingInstruction(target, data);
+		parent.appendChild(pi);
+	}
+
+	public void setDocumentLocator(Locator locator){}
+
+
+	public void startDocument() throws SAXException{}
+	public void endDocument() throws SAXException{}
+	public void startPrefixMapping(String prefix, String uri)throws SAXException{}
+	public void endPrefixMapping(String prefix)throws SAXException{}
+	public void ignorableWhitespace(char ch[], int start, int length)throws SAXException{}
+	public void skippedEntity(String name)throws SAXException{}
+
+
 	
 }
