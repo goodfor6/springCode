fdf0ff4b3b8ee97e77da260bf6df39d00296f72d
goodfor6 <1015401377@qq.com>
2017-08-29 23:26:32 +0800

    简单的转换错误监听器

--- src/org/springframework/core/util/xml/SimpleTransformErrorListener.java
+++ src/org/springframework/core/util/xml/SimpleTransformErrorListener.java
@@ -1,5 +1,28 @@
 package org.springframework.core.util.xml;
 
-public class SimpleTransformErrorListener {
+import javax.xml.transform.ErrorListener;
+import javax.xml.transform.TransformerException;
 
+import org.apache.commons.logging.Log;
+
+public class SimpleTransformErrorListener implements ErrorListener {
+	
+	private final Log logger;
+	
+	public SimpleTransformErrorListener(Log logger){
+		this.logger = logger;
+	}
+	
+	public void warning(TransformerException ex) throws TransformerException{
+		logger.warn("XSLT transformation warning",ex);
+	}
+	
+	public void error(TransformerException ex)throws TransformerException{
+		logger.error("XSLT transformation error",ex);
+	}
+	
+	public void fatalError(TransformerException ex) throws TransformerException{
+		throw ex;
+	}
+	
 }
