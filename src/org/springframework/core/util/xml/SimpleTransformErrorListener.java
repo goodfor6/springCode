06aac48771b5c95a6be561dda806064d77aea64e
goodfor6 <1015401377@qq.com>
2017-08-30 16:26:02 +0800

    megry

--- src/org/springframework/core/util/xml/SimpleTransformErrorListener.java
+++ src/org/springframework/core/util/xml/SimpleTransformErrorListener.java
@@ -2,6 +2,8 @@ package org.springframework.core.util.xml;
 
 import org.apache.commons.logging.Log;
 import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import javax.xml.transform.TransformerException;
 
@@ -9,18 +11,34 @@ public class SimpleTransformErrorListener implements ErrorHandler {
 
     private final Log logger;
 
-    @Override
+
+    public SimpleTransformErrorListener(Log logger) {
+        this.logger = logger;
+    }
+
     public void warning(TransformerException ex) throws TransformerException {
         logger.warn("XSLT transformation warning", ex);
     }
 
-    @Override
     public void error(TransformerException ex) throws TransformerException {
         logger.error("XSLT transformation error", ex);
     }
 
-    @Override
     public void fatalError(TransformerException ex) throws TransformerException {
         throw ex;
+    }
+
+    public void warning(SAXParseException exception) throws SAXException {
+
+    }
+
+    @Override
+    public void error(SAXParseException exception) throws SAXException {
+
+    }
+
+
+    public void fatalError(SAXParseException exception) throws SAXException {
+
     }
 }
